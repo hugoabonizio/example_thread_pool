1f83e33651e349f43f1356d27da9bd68cd2456bd
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-04-16 16:30:53 +0000

    Add initial tests to expected behavior of thread pool

--- spec/my_thread_pool_spec.rb
+++ spec/my_thread_pool_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe MyThreadPool do
-  it 'has a version number' do
-    expect(ExampleThreadPool::VERSION).not_to be nil
-  end
+#   it 'has a version number' do
+#     expect(ExampleThreadPool::VERSION).not_to be nil
+#   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+#   it 'does something useful' do
+#     expect(false).to eq(true)
+#   end
+  before do
+    @pool = MyThreadPool.new(size: 2)
+  end
+  
+  it 'should perform a task' do
+    counter = 1
+    @pool.perform do
+      counter += 1
+    end
+    @pool.join
+    expect(counter).to eq 2
   end
 end
