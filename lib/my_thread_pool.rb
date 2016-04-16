d5d3298164443c9e01ad3cce78f9a1f3e0dc04e9
Hugo Abonizio <hugo_abonizio@hotmail.com>
2016-04-16 16:31:10 +0000

    Implement initial thread pool

--- lib/my_thread_pool.rb
+++ lib/my_thread_pool.rb
@@ -1,5 +1,23 @@
 require "my_thread_pool/version"
 
-module MyThreadPool
-  # Your code goes here...
+class MyThreadPool
+  def initialize(size: 2)
+    @queue = Queue.new
+    @threads = Array.new(size) do
+      Thread.new do
+        while task = @queue.pop and !task.nil?
+          task.call
+        end
+      end
+    end
+  end
+  
+  def perform(&block)
+    @queue.push block
+  end
+  
+  def join
+    @threads.size.times { @queue.push nil }
+    @threads.map(&:join)
+  end
 end
--- lib/my_thread_pool/version.rb
+++ lib/my_thread_pool/version.rb
@@ -1,3 +1,3 @@
-module MyThreadPool
+class MyThreadPool
   VERSION = "0.1.0"
 end
